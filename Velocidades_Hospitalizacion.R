5cd0cf7566e261f259434f26f21ee7bde027940f
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2021-05-19 08:29:18 -0600

    Update Velocidades_Hospitalizacion.R

--- Velocidades_Hospitalizacion.R
+++ Velocidades_Hospitalizacion.R
@@ -15,12 +15,16 @@ finite.differences <- function(data) {
   return(fdx)
 }
 
-# carga los daots de https://geovision.uned.ac.cr/oges/
+# carga los datos de https://geovision.uned.ac.cr/oges/
 # noten la fecha en el directorio y nombre de archivo
+# cambiar la fecha acordemente para cargar los últimos datos
 data <- read.csv("https://geovision.uned.ac.cr/oges/archivos_covid/2021_05_18/05_18_21_CSV_GENERAL.csv", sep=';')
+
 # hospitalizaciones en la columna 30
 hosp <-data[,30]
+
 # calcula la derivada numérica
 deriv <- finite.differences(hosp)
+
 # grafica la derivada usando un filtro de gauss de 7 días
 plot(smth(deriv,window = 7,method = "gaussian"),type = 'l', col="red",xlab="Días", ylab="Velocidad", main="Velocidades de Hospitalización")

95a2fe3ee427db756a4d52956c768c8eab793f43
Tomás de Camino Beck, Ph.D <tomas.decamino@gmail.com>
2021-05-25 14:13:39 -0600

    Add files via upload

--- R_Efectivo.R
+++ R_Efectivo.R
@@ -1,22 +1,16 @@
 # Tomás de Camino Beck
 # tomas.decamino@gmail.com
 
-#para calcular tasa de contagio según fórmula del CCP
-# https://ccp.ucr.ac.cr/index.php/tasa-r-covid-19.html
-
 # se utiliza el paquete smoother para aplicar
 # filtro de gauss al resultado
 library(smoother)
 
-
+#función de ponderación
 weighted<-function(a){
   return(0.12 * exp(-0.0665*(a - 2) + (0.0048/0.2236) * (1 - exp(0.2236 * a))))
 }
 
-effective.R<-function(d,t,u){
-  d[t]/sum(rev(mapply(weighted, 2:(u+1) )* d[(t-u+1):t]))
-}
-
+#para cada tiempo hace sumatoria ponderada u días atrás
 effective.R<-function(d,u){
   n <- length(d)
   r <- vector(length = n)
@@ -33,6 +27,7 @@ data <- read.csv("https://geovision.uned.ac.cr/oges/archivos_covid/2021_05_24/05
 # casos nuevos positivos en la columna 3
 hosp <-data[,3]
 
+#calcula el R efectivo
 R <- effective.R(hosp,15)
 
 # grafica la derivada usando un filtro de gauss de 7 días
